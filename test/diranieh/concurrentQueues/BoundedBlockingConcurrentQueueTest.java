2f1c1ed9ece4a9658b744c74f83139cb5d3ac157
Yazan Diranieh <yazanmd@yahoo.com>
2020-01-26 14:50:59 +0000

    Initial implementation for a non-blocking concurrent list

--- test/diranieh/concurrentQueues/BoundedBlockingConcurrentQueueTest.java
+++ test/diranieh/concurrentQueues/BoundedBlockingConcurrentQueueTest.java
@@ -1,5 +1,18 @@
 package diranieh.concurrentQueues;
 
-class BoundedBlockingConcurrentQueueTest {
+class BoundedBlockingConcurrentQueueTest implements SequentialQueueTests, ConcurrentQueueTests {
 
+    @Override
+    public Queue<Integer> createQueue(int capacity) {
+        return new BoundedBlockingConcurrentQueue<Integer>(capacity);
+    }
+
+    @Override
+    public Queue<Integer> createAndPopulateQueue(int capacity, Iterable<Integer> items) throws InterruptedException {
+        BoundedBlockingConcurrentQueue<Integer> queue = new BoundedBlockingConcurrentQueue<>(capacity);
+        for (Integer item: items) {
+            queue.enqueue(item);
+        }
+        return queue;
+    }
 }
