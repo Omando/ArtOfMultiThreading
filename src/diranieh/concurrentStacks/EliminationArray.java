0c8224921bc33aad78169bef793a9634dd7522a6
Yazan Diranieh <yazanmd@yahoo.com>
2020-02-15 16:46:42 +0000

    initial implementation 2

--- src/diranieh/concurrentStacks/EliminationArray.java
+++ src/diranieh/concurrentStacks/EliminationArray.java
@@ -1,4 +1,14 @@
 package diranieh.concurrentStacks;
 
+/**
+ *
+ * We want two threads, one pushing and one popping, to coordinate and cancel out,
+ * but must avoid a situation in which a thread coordinates and cancels out more
+ * than one thread. This is done by implementing the EliminationArray using
+ * coordination structures called exchangers, objects that allow exactly two threads
+ * (and no more) to rendezvous and exchange values
+ *
+ * @param <E> the type of elements in this array
+ */
 public class EliminationArray<E> {
 }
