e96e43eb31f7046f7715388f6a543cc4b5dee15a
Yazan Diranieh <yazanmd@yahoo.com>
2020-03-07 21:52:12 +0000

    Initial implmenentation - inner class Node

--- src/diranieh/concurrentHashing/lockFree/BucketList.java
+++ src/diranieh/concurrentHashing/lockFree/BucketList.java
@@ -1,5 +1,28 @@
 package diranieh.concurrentHashing.lockFree;
 
+import java.util.concurrent.atomic.AtomicMarkableReference;
+
 public class BucketList<E> {
-    // TODO
+    static final int HI_MASK = 0X00800000;
+    static final int LOW_MASK = 0X00FFFFFF;
+
+    private static class Node<E> {
+        int key;
+        E value;
+        AtomicMarkableReference<Node> next;
+
+        // Normal constructor for all nodes except the sentinel
+        Node(int key, E value) {
+            this.key = key;
+            this.value = value;
+            this.next = new AtomicMarkableReference<>(null, false);
+        }
+
+        // Sentinel constructor
+        Node(int key) {
+            this.key = key;
+            this.value = null;
+            this.next = new AtomicMarkableReference<>(null, false);
+        }
+    }
 }
