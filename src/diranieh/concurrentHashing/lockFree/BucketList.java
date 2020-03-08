b4bfb505cf1910525f46cce5aca8a9b780162ad7
Yazan Diranieh <yazanmd@yahoo.com>
2020-03-08 22:34:14 +0000

    Delta implmenentation 1 - making ordinary and sentinel keys

--- src/diranieh/concurrentHashing/lockFree/BucketList.java
+++ src/diranieh/concurrentHashing/lockFree/BucketList.java
@@ -2,14 +2,17 @@ package diranieh.concurrentHashing.lockFree;
 
 import java.util.concurrent.atomic.AtomicMarkableReference;
 
+import static java.lang.Integer.reverse;
+
 public class BucketList<E> {
-    static final int HI_MASK = 0X00800000;
-    static final int LOW_MASK = 0X00FFFFFF;
+    static final int HI_MASK  = 0X80000000;
+    static final int MASK = 0X00FFFFFF;
+    Node<E> head;
 
     private static class Node<E> {
         int key;
         E value;
-        AtomicMarkableReference<Node> next;
+        AtomicMarkableReference<Node<E>> next;
 
         // Normal constructor for all nodes except the sentinel
         Node(int key, E value) {
@@ -24,5 +27,27 @@ public class BucketList<E> {
             this.value = null;
             this.next = new AtomicMarkableReference<>(null, false);
         }
+    }
+
+    public BucketList() {
+        // Create an empty list
+        head = new Node<>(0);     // Create a sentinel node to bucket 0
+
+        // Tail is sentinel node to larget possible bucket index
+        head.next = new AtomicMarkableReference<Node<E>>(new Node<E>(Integer.MAX_VALUE), false);
+    }
+
+    // Creating keys for ordinary and sentinel nodex
+    private int makeOrdinaryKey(E item) {
+        // Take lowest three bytes so as to always have a positive value
+        int lowestThreeBytes = item.hashCode() & MASK;
+        int lowestThreeBytesWithMSBOn = lowestThreeBytes | HI_MASK;
+        return reverse(lowestThreeBytesWithMSBOn);
+    }
+
+    private int makeSentinelKey(E item) {
+        // Take lowest three bytes so as to always have a positive value
+        int lowestThreeBytes = item.hashCode() & MASK;
+        return reverse(lowestThreeBytes);
     }
 }
