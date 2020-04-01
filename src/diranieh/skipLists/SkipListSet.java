b100ab7805e2f75e38042d4faa2a97e48100ebd9
Yazan Diranieh <yazanmd@yahoo.com>
2020-04-01 01:19:28 +0100

    concurrent skip list - delta change 4 - added add() method

--- src/diranieh/skipLists/SkipListSet.java
+++ src/diranieh/skipLists/SkipListSet.java
@@ -27,16 +27,18 @@ public class SkipListSet<E extends Comparable<E>> {
     }
 
     private final static int MAX_LEVELS = 32;
-    private Node<E> sentinel;
+    private final Random random;
+    private final Node<E> sentinel;
+    private final Node<E>[] predecessors;
     private int height;         // the maximum height of any element
     private int count;          // number of elements stored in tje skip list
-    private Random random;
 
     public SkipListSet() {
         random = new Random();
         count = 0;
         height = 0;
         sentinel = new Node<E>(null, MAX_LEVELS);
+        predecessors = (Node<E>[])new Node[MAX_LEVELS];
     }
 
     // Each step (right or down) takes a constant time. The expected running time
@@ -49,8 +51,52 @@ public class SkipListSet<E extends Comparable<E>> {
         return predecessor.next[0] == null? null : predecessor.next[0].item;
     }
 
+    // Search for item and then splice the item into a few lists Lo...Lk where k is
+    // selected using getNewNodeHeight.
     public boolean add(E item) {
-        return false;
+        Node<E> predecessor = sentinel;     // Start from the sentinel
+        int currentLevel = height;
+        int compareResult = 0;
+
+        // Iterate over all levels starting from the maximum available height
+        while (currentLevel <= 0) {
+
+            // Move right while the next item is less than the search item
+            while (predecessor.next[currentLevel] != null &&
+                    (compareResult = predecessor.next[currentLevel].item.compareTo(item)) < 0)
+                predecessor = predecessor.next[currentLevel];
+
+            // We cannot move right any more. If item is found, then nothing else to do
+            if (predecessor.next[currentLevel] != null && compareResult == 0)
+                return false;
+
+            // Item was not found at this level. Store the predecessor and go down one
+            // level and repeat until we reach level zero
+            predecessors[currentLevel] = predecessor;
+            currentLevel--;
+        }
+
+        // Item not found. We are now at level 0. Create the new node with some random height
+        int newNodeHeight = getNewNodeHeight();
+        Node<E> newNode = new Node<>(item, newNodeHeight);
+
+        // If new item's height is > the current maximum height, the add additional
+        // entries that point to the sentinel (see figure in document for adding 3.5)
+        while (height < newNode.height())
+            predecessors[++height] = sentinel;      // Increase height
+
+        // see DSALG_LinkedList document -> adding 3.5, page 8 & 9
+        for (int i = 0; i < newNode.next.length; i++) {
+            newNode.next[i] = predecessors[i].next[i];      // adding 3.5, top figure
+            predecessors[i].next[i] = newNode.next[i];      // adding 3.5, bottom figure
+        }
+
+        count++;        // increment the number of elements in the list
+        return true;
+    }
+
+    public boolean remove(E item) {
+        return false;   // continue here
     }
 
     // Simulate tossing a coin: given a 32-bit random integer, a bit with value 1 represents
@@ -60,17 +106,18 @@ public class SkipListSet<E extends Comparable<E>> {
     // number represents a tails, and the location of that first 0 is the height.
     // Note Maximum height returned is 32 since the random number is a 32-bit integer
     private int getNewNodeHeight() {
-        int z = random.nextInt();
-        int height = 0;
-        int m = 1;      // bit 1
+        int height = 0;     // Initial height is 0
+        int nthBit = 1;     // Start at bit 1
+        int randomNumber = random.nextInt();
 
-        // Repeat while bit m of number z is 1 (heads)
-        while ((z & m) != 0) {
-            height++;       // Increment height
-            m <<= 1;        // Left shift m by one 1
+        // Repeat while the nth bit of randomNumber is 1 (heads)
+        while ((randomNumber & nthBit) != 0) {
+            height++;           // Increment height
+            nthBit <<= 1;       // Left shift m by one 1 to move to the next more significant bit
         }
 
-        // Bit m in z is 0 (tails). Its location is the required height
+        // The nth bit of randomNumber is 0 (tails). The location of the nth bit
+        // is the required height
         return height;
     }
 
@@ -89,9 +136,6 @@ public class SkipListSet<E extends Comparable<E>> {
             // We cannot move right any more. Move one level down
             currentLevel--;
         }
-
         return predecessor;
     }
-
-
 }
