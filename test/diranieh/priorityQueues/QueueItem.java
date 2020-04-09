9968ddb0f563a77be6eb52c17e6f611a18ca5fcd
Yazan Diranieh <yazanmd@yahoo.com>
2020-04-09 23:41:29 +0100

    array based thread-safe priority queue - testing infrastructure

---
+++ test/diranieh/priorityQueues/QueueItem.java
@@ -0,0 +1,19 @@
+package diranieh.priorityQueues;
+
+public class QueueItem {
+    private  String item;
+    private  int priority;
+
+    public QueueItem(String item, int priority) {
+        this.item = item;
+        this.priority = priority;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+}
