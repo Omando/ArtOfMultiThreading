3c4f344f1d083410ea3ba9566ca4f30671997934
Yazan Diranieh <yazanmd@yahoo.com>
2020-04-11 23:20:58 +0100

    tree based thread-safe priority queue - placeholder

---
+++ src/diranieh/priorityQueues/TreeBasedBoundedPriorityQueue.java
@@ -0,0 +1,13 @@
+package diranieh.priorityQueues;
+
+public class TreeBasedBoundedPriorityQueue<E> implements PriorityQueue<E> {
+    @Override
+    public void add(E item, int priority) {
+
+    }
+
+    @Override
+    public E removeMin() {
+        return null;
+    }
+}
