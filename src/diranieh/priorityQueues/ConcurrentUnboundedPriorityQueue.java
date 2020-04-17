4a9c1074fb4b521345eb8d97db44c8dfb8e43297
Yazan Diranieh <yazanmd@yahoo.com>
2020-04-17 23:35:15 +0100

    tree based unbounded concurrent priority queue - skeletal implementation

--- src/diranieh/priorityQueues/ConcurrentUnboundedPriorityQueue.java
+++ src/diranieh/priorityQueues/ConcurrentUnboundedPriorityQueue.java
@@ -1,6 +1,9 @@
 package diranieh.priorityQueues;
 
 public class ConcurrentUnboundedPriorityQueue<E> implements PriorityQueue<E> {
+    private static class Node<E> {
+        private E item;
+    }
     @Override
     public void add(E item, int priority) {
 
