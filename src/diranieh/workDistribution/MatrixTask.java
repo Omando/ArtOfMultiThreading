7c4764db12c329d15e8f6817759778f7abca8877
Yazan Diranieh <yazanmd@yahoo.com>
2020-04-23 01:05:13 +0100

    Concurrent matrix addition and multiplication - add and multiply driver

--- src/diranieh/workDistribution/MatrixTask.java
+++ src/diranieh/workDistribution/MatrixTask.java
@@ -7,7 +7,9 @@ import java.util.concurrent.Future;
 
 /* All matrices are assumed to be n x n */
 public class MatrixTask implements IMatrixTask {
-    private static final ExecutorService _executor = Executors.newFixedThreadPool(5);
+    // Creates a thread pool that creates new threads as needed, but will reuse
+    // previously constructed threads when they are available.
+    private static final ExecutorService _executor = Executors.newCachedThreadPool();
 
     @Override
     public Matrix add(Matrix a, Matrix b) throws ExecutionException, InterruptedException {
@@ -17,7 +19,7 @@ public class MatrixTask implements IMatrixTask {
         Matrix result =  new Matrix(n);
 
         // Add a and b matrices concurrently
-        Future<?> future = _executor.submit( new AddMatrixTask(a, b, result));
+        Future<?> future = _executor.submit( new AddMatrixTask(a, b, result, _executor));
         future.get();
         return result;
     }
@@ -29,7 +31,7 @@ public class MatrixTask implements IMatrixTask {
         Matrix result =  new Matrix(n);
 
         // Multiply matrices a and b concurrently
-        Future<?> future = _executor.submit( new MultiplyMatrixTask(a, b, result));
+        Future<?> future = _executor.submit( new MultiplyMatrixTask(a, b, result, _executor));
         future.get();
         return result;
     }
