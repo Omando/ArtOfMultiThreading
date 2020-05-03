7e14e78eb62ac5a9112804865a22cdca9b6cd107
Yazan Diranieh <yazanmd@yahoo.com>
2020-05-04 00:17:07 +0100

    exponential back off lock - not tested

---
+++ src/diranieh/locks/ExponentialBackoff.java
@@ -0,0 +1,25 @@
+package diranieh.locks;
+
+import java.util.Random;
+
+public class ExponentialBackoff {
+    private final int _maxDelay;
+    private final Random _random;
+    private int _currentDelay;
+
+    public ExponentialBackoff(int min, int max) {
+        _currentDelay = min;    // initial minimum delay. Makes no sense if back off it too short
+        _maxDelay = max;        // prevents threads from backing off for very long times
+        _random = new Random(); // random delay between zero and current delay
+    }
+
+    public void backOff() throws InterruptedException {
+        // Initially we start with a delay up to the given minimum, then exponentially
+        // increment the delay by a power of 2 for each successive back off
+        Thread.sleep(_currentDelay);
+
+        // Setup delay for next backoff
+        int limit = Math.min(_maxDelay, _currentDelay * 2);
+        _currentDelay = _random.nextInt(limit);
+    }
+}
