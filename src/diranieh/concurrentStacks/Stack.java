19f0fb21ed93937ada7ce8650834125728f91922
Yazan Diranieh <yazanmd@yahoo.com>
2020-05-16 19:17:13 +0100

    Added additional comments

--- src/diranieh/concurrentStacks/Stack.java
+++ src/diranieh/concurrentStacks/Stack.java
@@ -1,5 +1,9 @@
 package diranieh.concurrentStacks;
 
+/**
+ * The {@code Stack} class represents a last-in-first-out (LIFO) stack of objects
+ * @param <E> the type of elements in the stack
+ */
 public interface Stack<E> {
     void push(E item);
     E pop();
