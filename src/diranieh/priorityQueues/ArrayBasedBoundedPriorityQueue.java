7cbed19d41215613136fa608b07eede40e2bd4f5
Yazan Diranieh <yazanmd@yahoo.com>
2020-05-22 00:47:46 +0100

    Added additional comments

--- src/diranieh/priorityQueues/ArrayBasedBoundedPriorityQueue.java
+++ src/diranieh/priorityQueues/ArrayBasedBoundedPriorityQueue.java
@@ -2,7 +2,15 @@ package diranieh.priorityQueues;
 
 import diranieh.concurrentStacks.ConcurrentLockFreeStack;
 
-/* Array-based, thread-safe bounded priority queue */
+/** Array-based, thread-safe bounded priority queue.
+ * A bounded-range priority queue is one where each item’s score (priority)
+ * is taken from the range 0, …, m-1 where m is the range. An unbounded-range
+ * priority queue is one where scores are taken from a very large set, say
+ * 32-bit integers, or  floating-point values
+ * .
+ * This implementation we use an array of ConcurrentLockFreeStack<E> as the
+ * underlying bounded pool with range of priorities from 0 to m-1
+ * */
 public class ArrayBasedBoundedPriorityQueue<E> implements PriorityQueue<E> {
     private final int priorityRange;
     private final ConcurrentLockFreeStack<E>[] pool;
